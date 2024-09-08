a180b7a28909ff8e63af9b996d14298397158275
ashusharma97 <ashutosh.sharma@thoughtworks.com>
2024-09-08 20:07:30 +0530

    Update backend.tf

--- session_5/backend.tf
+++ session_5/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "iac-lab-sr-tfstate"
+    bucket = "iac-lab-aashu-tfstate"
     key    = "ap-south-1/iac-lab-aashu-tfstate/terraform.tfstate"
     region = "ap-south-1"
 
