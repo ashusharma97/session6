222988b7434ae8490012b3b740dbfce13c1438f2
ashusharma97 <ashutosh.sharma@thoughtworks.com>
2024-09-08 19:58:49 +0530

    Update backend.tf

--- session_5/backend.tf
+++ session_5/backend.tf
@@ -1,10 +1,10 @@
 terraform {
   backend "s3" {
     bucket = "iac-lab-sr-tfstate"
-    key    = "ap-southeast-2/iac-lab-sr/terraform.tfstate"
+    key    = "ap-south-1/iac-lab-aashu-tfstate/terraform.tfstate"
     region = "ap-southeast-2"
 
-    dynamodb_table = "iac-lab-sr-tfstate-locks"
+    dynamodb_table = "iac-lab-aashu-tfstate-locks"
     encrypt        = true
   }
 }
